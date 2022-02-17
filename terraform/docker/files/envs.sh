c41eecf709e53fa8b7e08ad62442359bc114d767
Wais,Erhard (IT INF) BIG-AT-V <erhard.wais@boehringer-ingelheim.com>
2022-02-17 17:15:46 +0100

    setup files

---
+++ terraform/docker/files/envs.sh
@@ -0,0 +1,8 @@
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_REGION="${AWS_DEFAULT_REGION}"
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export HTTPS_PROXY=
+export HTTP_PROXY=
+export https_proxy=
+export http_proxy=
